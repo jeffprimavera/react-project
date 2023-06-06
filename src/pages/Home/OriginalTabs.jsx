e181985fcf34a4c166c69674acd4a3cde6b48524
Jeff <jeffrey.web.ph@tripleonetech.net>
2023-06-06 23:34:04 +0800

    Update HP

--- src/pages/Home/OriginalTabs.jsx
+++ src/pages/Home/OriginalTabs.jsx
@@ -1,5 +1,6 @@
 import { OriginalGames } from './ImageData'
 import {Link} from 'react-router-dom'
+import { IconPlayerPlayFilled } from '@tabler/icons-react';
 
 const OriginalTabsGame = () => {
     return (
@@ -10,18 +11,31 @@ const OriginalTabsGame = () => {
                     <h3>Bet Originals</h3>
                 </div>
                 <div className='game__wrapper relative overflow-hidden w-full'>
-                    <div className='grid grid-cols-6 items-center gap-6'>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
-                        <div className='game__box'>1</div>
+                    <div className='grid grid-cols-6 m-0 p-0'>
+
+                        { OriginalGames.map((item) => {
+                            return (
+                                <div className='game__box'>
+                                    <figure key={item.id} className='relative'>
+                                        <div className='img__thumb'>
+                                            <img src={ item.img } alt={item.title} />
+                                        </div> 
+                                        <figcaption className='flex justify-center items-center opacity-0 invisible absolute top-0 left-0 w-full h-full cursor-pointer transition-all'>
+                                            <div>
+                                                <Link to={item.link}>
+                                                    <p>{ item.gamename }</p>
+                                                    <span>
+                                                        <IconPlayerPlayFilled color='' /> 
+                                                    </span>
+                                                </Link>
+                                            </div>
+                                        </figcaption>
+                                    </figure>
+                                </div>
+                            )
+                        }
+                        )}
+                        
                     </div>
                 </div>
             </div>
