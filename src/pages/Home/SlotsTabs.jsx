e95b89653e5ae8476a7a5ed8b75e35b5bafa57d1
Jeff <jeffrey.web.ph@tripleonetech.net>
2023-06-07 18:33:37 +0800

    update hp

--- src/pages/Home/SlotsTabs.jsx
+++ src/pages/Home/SlotsTabs.jsx
@@ -47,7 +47,7 @@ const SlotsTabsGame = () => {
                 <div className='game__wrapper relative w-full'>
 
                 <Swiper
-                spaceBetween={10}
+                spaceBetween={15}
                 slidesPerView={6}
                 loop={true}
                 navigation={{
@@ -85,27 +85,7 @@ const SlotsTabsGame = () => {
                         <p>Loading...</p>
                     )}
                     
-                </Swiper>
-
-                        {/* <div className='game__box' key={i}>
-                                <figure key={game.game_unique_id} className='relative rounded-xl overflow-hidden'>
-                                    <div className='img__thumb'>
-                                        <img className='w-full' src={game.image_path.en} alt={item.title} />
-                                    </div> 
-                                    <figcaption className='flex justify-center items-center opacity-0 invisible absolute top-0 left-0 w-full h-full cursor-pointer transition-all'>
-                                        <div className='text-center text-lg '>
-                                            <Link to={item.link} className='flex justify-between flex-col h-[225px]'>
-                                                <p className='text-white block'>{ item.gamename }</p>
-                                                <span className='flex justify-center items-center w-20 h-20 rounded-full'>
-                                                    <IconPlayerPlayFilled className='text-white' /> 
-                                                </span>
-                                                <p className='text-white block text-xs link__color1'>{ item.owner }</p>
-                                            </Link>
-                                        </div>
-                                    </figcaption>
-                                </figure>
-                            </div> */}
-                    
+                </Swiper>  
 
                 </div>
 
