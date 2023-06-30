9f4c9802f16d0d0ee6500f182db1f51e2d77e19c
Jeff <jeffrey.web.ph@tripleonetech.net>
2023-06-30 18:38:47 +0800

    Add Login

--- src/Header/Login/index.jsx
+++ src/Header/Login/index.jsx
@@ -17,7 +17,7 @@ const customStyles = {
         right: 0,
         bottom: 0,
         backgroundColor: 'rgb(0 0 0 / 75%)'
-    },
+    }, 
     content: {
         top: "50%",
         left: "50%",
